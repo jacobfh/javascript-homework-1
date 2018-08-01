3f04dffacf022ec3cbe52da342062b6916deb5f3
Jacobfh <jfh708@gmail.com>
2018-07-31 17:38:44 -0700

    far to kel complete

--- homework.js
+++ homework.js
@@ -63,6 +63,10 @@ function celsiusToFahrenheit (temp) {
 // Absolute zero (0 K) is equivalent to −273.15 C.
 // 1 degree Kelvin equals 1 degree Celsius.
 
+function fahrenheitToKelvin (temp){
+    return fahrenheitToCelsius + 273.15
+}
+
 // 8. Create a function called lesser that takes two numbers as
 // arguments and returns the lesser of them. This function should
 // use an if/else statement.
